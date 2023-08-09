2d17d7a1cf0bcd1c710b4cea1f16f790126c344d
Rawle Becerril <rawlesxm@gmail.com>
2023-08-08 21:10:18 -0400

    handling events

--- cwm-react/src/components/ListGroup.tsx
+++ cwm-react/src/components/ListGroup.tsx
@@ -1,12 +1,23 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
 
     const items = ["New York", "Tokyo", "London", "Paris"]
 
+    const handleClick = (event: MouseEvent) => console.log(event)
+
   return (<>
     <h1>List</h1>
     {items.length === 0 ? <p>No Item Found</p> : null}
-    <ul className="list-group">
-    {items.map(item => <li key={item}>{item}</li>)}
+    <ul className="list-group-item">
+    {items.map((item, index) => 
+    <li 
+    key={item} 
+    onClick={handleClick}>
+      
+      {item}
+      
+      </li>)}
     </ul>
   </>);
 }
