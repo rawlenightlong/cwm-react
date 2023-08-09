31807cb3b95448cdba95f819b55b9df5371e0c09
Rawle Becerril <rawlesxm@gmail.com>
2023-08-08 21:50:53 -0400

    passing children to components

--- cwm-react/src/App.tsx
+++ cwm-react/src/App.tsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import ListGroup from './components/ListGroup'
+import Alert from './components/Alert'
 function App() {
 
-  const items = ["New York", "Tokyo", "London", "Paris"]
-  const handleSelectItem = (item: string) => {console.log(item)}
+  // const items = ["New York", "Tokyo", "London", "Paris"]
+  // const handleSelectItem = (item: string) => {console.log(item)}
 
   return <div>
-    <ListGroup items = {items} heading={"Cities"} onSelectItem={handleSelectItem}/>
+    {/* <ListGroup items = {items} heading={"Cities"} onSelectItem={handleSelectItem}/> */}
+    <Alert>
+      Hello <span>World</span>
+    </Alert>
   </div>
 }
 
