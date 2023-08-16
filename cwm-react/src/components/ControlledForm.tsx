b6b722928704a0a5f0caea297457d848b9c9feae
Rawle Becerril <rawlesxm@gmail.com>
2023-08-15 21:34:26 -0400

    controlled forms

--- cwm-react/src/components/ControlledForm.tsx
+++ cwm-react/src/components/ControlledForm.tsx
@@ -1,9 +1,15 @@
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useState } from 'react'
 
 const ControlledForm = () => {
 
+
+    const [person, setPerson] = useState({
+        name: "",
+        age: ''
+    })
     const handleSubmit = (event: FormEvent) => {{
         event.preventDefault()
+        console.log(person)
 
     }}
 
@@ -12,12 +18,12 @@ const ControlledForm = () => {
     <form onSubmit={handleSubmit}>
         <div className="mb-3">
             <label htmlFor="" className="form-label"> Name </label>
-            <input  id="name" type="text" className="form-control" />
+            <input onChange={(e) => {setPerson({...person, name: e.target.value})}} id="name" type="text" className="form-control" value={person.name} />
         </div>
 
         <div className="mb-3">
             <label htmlFor="" className="form-label"> Age </label>
-            <input type="number" id="age" className="form-control" />
+            <input onChange={(e)=>{setPerson({...person, age: parseInt(e.target.value)})}}type="number" id="age" className="form-control" value={person.age}/>
         </div>
         <button className="btn btn-primary" type="submit">Submit</button>
     </form>
