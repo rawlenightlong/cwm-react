bf99917c1fe2a3ba1fb07f3bb8340a8606eb4604
Rawle Becerril <rawlesxm@gmail.com>
2023-08-15 22:11:36 -0400

    zod validation

--- cwm-react/src/components/ControlledForm.tsx
+++ cwm-react/src/components/ControlledForm.tsx
@@ -1,13 +1,18 @@
 import React, { FormEvent, useState } from 'react'
 import {FieldValues, useForm} from "react-hook-form"
-interface FormData {
-    name: string,
-    age: number
-}
+import {z} from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
+
+const schema = z.object({
+    name: z.string().min(3),
+    age: z.number({invalid_type_error: "Age field is required" }).min(18, { message: "Must be 18"})
+})
+
+type FormData = z.infer<typeof schema>
 
 const ControlledForm = () => {
 
-    const {register, handleSubmit, formState: {errors}} = useForm<FormData>()
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>({resolver: zodResolver(schema)})
     const onSubmit = (data: FieldValues) => {
     }
 
@@ -18,21 +23,22 @@ const ControlledForm = () => {
         <div className="mb-3">
             <label htmlFor="" className="form-label"> Name </label>
             <input 
-            { ...register("name", { required: true, minLength: 3 })}
+            { ...register("name")}
             id="name" 
             type="text" 
             className="form-control"/>
-            {errors.name?.type === "required" && <p className="text-danger">The name field is required</p>}
-            {errors.name?.type === "minLength" && <p className="text-danger">The name must be at least 3 characters</p>}
+            {errors.name && <p className="text-danger">{errors.name.message}</p>}
+           
         </div>
 
         <div className="mb-3">
             <label htmlFor="" className="form-label"> Age </label>
             <input 
-            {...register("age")}
+            {...register("age", {valueAsNumber: true})}
             type="number" 
             id="age" 
             className="form-control"/>
+            {errors.age && <p className="text-danger">{errors.age.message}</p>}
         </div>
         <button className="btn btn-primary" type="submit">Submit</button>
     </form>
