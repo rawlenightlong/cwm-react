7b05c93d8ca78c2955c92428ad72f9a9b7d90d36
Rawle Becerril <rawlesxm@gmail.com>
2023-08-15 22:00:23 -0400

    applying validation

--- cwm-react/src/components/ControlledForm.tsx
+++ cwm-react/src/components/ControlledForm.tsx
@@ -1,11 +1,14 @@
 import React, { FormEvent, useState } from 'react'
 import {FieldValues, useForm} from "react-hook-form"
+interface FormData {
+    name: string,
+    age: number
+}
 
 const ControlledForm = () => {
 
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>()
     const onSubmit = (data: FieldValues) => {
-        console.log(data)
     }
 
 
@@ -15,10 +18,12 @@ const ControlledForm = () => {
         <div className="mb-3">
             <label htmlFor="" className="form-label"> Name </label>
             <input 
-            { ...register("name")}
+            { ...register("name", { required: true, minLength: 3 })}
             id="name" 
             type="text" 
             className="form-control"/>
+            {errors.name?.type === "required" && <p className="text-danger">The name field is required</p>}
+            {errors.name?.type === "minLength" && <p className="text-danger">The name must be at least 3 characters</p>}
         </div>
 
         <div className="mb-3">
